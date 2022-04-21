936f0688c1f1f427def8f9679469149e62ffd5c4
Nino Kodabande <nkodabande@suse.com>
2022-04-21 16:14:16 -0700

    wip

--- scripts/download/wsl.mjs
+++ scripts/download/wsl.mjs
@@ -14,4 +14,20 @@ export default async function main() {
     `https://github.com/rancher-sandbox/rancher-desktop-wsl-distro/releases/download/v${ v }/distro-${ v }.tar`,
     path.resolve(process.cwd(), 'resources', os.platform(), `distro-${ v }.tar`),
     { access: fs.constants.W_OK });
+
+  // Download host-resolver
+  // TODO(@Nino-k) once host-resolver stabilizes remove and add to wsl-distro
+  // resolver-0.1.0-beta2-windows-amd64.zip
+  const hostResolverVersion = '0.1.0-beta2'
+  // download vsock-host
+  await download(
+    `https://github.com/Nino-K/rancher-desktop-host-resolver/releases/download/${ v }/resolver-${ v }-windows-amd64.zip`,
+    path.resolve(process.cwd(), 'resources', os.platform(), `resolver-${ v }-windows-amd64.tar`),
+    { access: fs.constants.W_OK });
+
+  // download vsock-peer
+  await download(
+    `https://github.com/Nino-K/rancher-desktop-host-resolver/releases/download/${ v }/resolver-${ v }-linux-amd64.zip`,
+    path.resolve(process.cwd(), 'resources', os.platform(), `resolver-${ v }-linux-amd64.tar`),
+    { access: fs.constants.W_OK });
 }
